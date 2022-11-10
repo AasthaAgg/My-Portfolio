c88a2f6291e98ca483c271b04397527e02c8033a
Aastha Aggarwal <aastha.7090@gmail.com>
2022-11-11 00:23:03 +0530

    navigation menu added

---
+++ script.js
@@ -0,0 +1,23 @@
+/*===== ACTIVE AND REMOVE MENU =====*/
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click",() => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",() => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
+
+for(var i=0; i<navLinks.length; i++){
+    navLinks[i].addEventListener("click", function(){
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace("active");
+        this.classList.toggle("active");
+    });
+}
